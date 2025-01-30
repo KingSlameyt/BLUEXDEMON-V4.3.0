91ba516930c3cd17b095ff71e680b461af862135
KingSlameyt <107631912+KingSlameyt@users.noreply.github.com>
2025-01-30 07:42:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "492713876676"
+global.pairingNumber = "491752821649"
 
 
 
